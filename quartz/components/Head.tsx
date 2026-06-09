e787dcdeab4b80ac0847c7fad330120f7c1ddb98
guizacoatl <guizacoatl@gmail.com>
2026-06-09 22:52:49 +0200

    revert: remove pdf prefetch modifications

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -106,23 +106,7 @@ export default (() => {
             return resource
           }
         })}
-        <script dangerouslySetInnerHTML={{__html: `
-(function() {
-  function patchPdfLinks() {
-    document.querySelectorAll('a[href$=".pdf"]').forEach(function(link) {
-      link.setAttribute("data-no-prefetch", "true");
-      link.setAttribute("target", "_blank");
-      link.setAttribute("rel", "noopener noreferrer");
-    });
-  }
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", patchPdfLinks);
-  } else {
-    patchPdfLinks();
-  }
-  document.addEventListener("nav", patchPdfLinks);
-})();
-        `}} />
+
       </head>
     )
   }
